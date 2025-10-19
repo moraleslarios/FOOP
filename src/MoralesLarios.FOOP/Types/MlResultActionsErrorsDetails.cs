5684d193d22c38b1397a6fd71e59e793648f4bf0
MoralesLarios <moraleslarios@gmail.com>
2025-10-19 13:59:06 +0200

    AddFailValue inicial

---
+++ src/MoralesLarios.FOOP/Types/MlResultActionsErrorsDetails.cs
@@ -0,0 +1,120 @@
+﻿using System.Threading.Tasks;
+
+namespace MoralesLarios.OOFP.Types;
+
+public static class MlResultActionsErrorsDetails
+{
+
+    public static MlResult<T> GetDetail<T>(this MlErrorsDetails source, string key)
+    {
+        if ( ! source.Details.ContainsKey(key)) return source.AddError($"The key {key} does not exist in the details");
+        
+        var result = source.Details[key] is T value 
+                            ? MlResult<T>.Valid(value) 
+                            : source.AddError($"The key {key} does not contain a value of type {typeof(T).Name}");
+
+        return result;
+    }
+
+
+
+    public static MlResult<T> MergeErrorsDetailsIfFail<T>(this MlResult<T> source,
+                                                               MlResult<T> secondary)
+        => source.Match(
+                            fail : errorDetails => secondary.Match(
+                                                                        fail : errorDetails2 => errorDetails.Merge(errorDetails2),
+                                                                        valid: _             => errorDetails
+                                                                    ),
+                            valid: _ => source
+                        );
+
+    public static async Task<MlResult<T>> MergeErrorsDetailsIfFailAsync<T>(this MlResult<T> source,
+                                                                                MlResult<T> secondary)
+        => await source.MatchAsync(
+                                    failAsync : errorDetails => secondary.MatchAsync(
+                                                                                failAsync : errorDetails2 => errorDetails.Merge(errorDetails2).ToMlResultFailAsync<T>(),
+                                                                                validAsync: _             => errorDetails.ToMlResultFailAsync<T>()
+                                                                            ),
+                                    validAsync: _ => source.ToAsync()
+                                );
+
+    public static async Task<MlResult<T>> MergeErrorsDetailsIfFailAsync<T>(this Task<MlResult<T>> sourceAsync,
+                                                                                MlResult<T>       secondary)
+        => (await sourceAsync).MergeErrorsDetailsIfFail(secondary);
+
+    public static async Task<MlResult<T>> MergeErrorsDetailsIfFailAsync<T>(this Task<MlResult<T>> sourceAsync,
+                                                                                Task<MlResult<T>> secondary)
+        => (await sourceAsync).MergeErrorsDetailsIfFail(await secondary);
+
+    public static MlResult<T> MergeErrorsDetailsIfFailDiferentTypes<T,T2>(this MlResult<T > source,
+                                                                               MlResult<T2> secondary)
+        => source.Match(
+                                    fail : errorDetails => secondary.Match(
+                                                                                fail : errorDetails2 => errorDetails.Merge(errorDetails2),
+                                                                                valid: _             => errorDetails
+                                                                            ),
+                                    valid: _ => source
+                                );
+
+    public static async Task<MlResult<T>> MergeErrorsDetailsIfFailDiferentTypesAsync<T,T2>(this MlResult<T > source,
+                                                                                                MlResult<T2> secondary)
+        => await source.MatchAsync(
+                                    failAsync : errorDetails => secondary.MatchAsync(
+                                                                                        failAsync : errorDetails2 => errorDetails.Merge(errorDetails2).ToMlResultFailAsync<T>(),
+                                                                                        validAsync: _             => errorDetails.ToMlResultFailAsync<T>()
+                                                                            ),
+                                    validAsync: _ => source.ToAsync()
+                                );
+
+    public static async Task<MlResult<T>> MergeErrorsDetailsIfFailDiferentTypesAsync<T,T2>(this Task<MlResult<T>> sourceAsync,
+                                                                                                MlResult<T2>      secondary)
+        => (await sourceAsync).MergeErrorsDetailsIfFailDiferentTypes(secondary);
+
+    public static async Task<MlResult<T>> MergeErrorsDetailsIfFailDiferentTypesAsync<T,T2>(this Task<MlResult<T>>  sourceAsync,
+                                                                                                Task<MlResult<T2>> secondaryAsync)
+        => (await sourceAsync).MergeErrorsDetailsIfFailDiferentTypes(await secondaryAsync);
+
+
+    public static MlResult<T> GetDetailValue<T>(this MlErrorsDetails source)
+    {
+        if ( ! source.Details.ContainsKey(VALUE_KEY)) return source.AddError($"The key {VALUE_KEY} does not exist in the details");  //MlResult<T>.Fail($"The key {VALUE_KEY} does not exist in the details");
+        
+        var result = source.Details[VALUE_KEY] is T value 
+                            ? MlResult<T>.Valid(value) 
+                            : source.AddError($"The key {VALUE_KEY} does not contain a value of type {typeof(T).Name}");
+
+        return result;
+    }
+
+
+    public static MlResult<T> AddIfFailValue<T, TValue>(this MlResult<T> source,
+                                                             TValue      value)
+        => source.Match(
+                            fail : errorsDetails => errorsDetails.AddDetailValue(value),
+                            valid: _             => source
+                        );
+
+    public static async Task<MlResult<T>> AddIfFailValueAsync<T, TValue>(this MlResult<T> source,
+                                                                              TValue      value)
+        => await source.AddIfFailValue(value).ToAsync();
+
+    public static async Task<MlResult<T>> AddIfFailValue<T, TValue>(this Task<MlResult<T>> sourceAsync,
+                                                             TValue            value)
+    {
+        var result = await sourceAsync.MatchAsync(
+                                                    failAsync : errorsDetails => errorsDetails.AddDetailValue(value).ToMlResultFailAsync<T>(),
+                                                    validAsync: _             => sourceAsync
+                                                );
+        return result;
+    }
+
+
+
+    public static Task<MlResult<T>> GetDetailValueAsync<T>(this MlErrorsDetails source) => source.GetDetailValue<T>().ToAsync();
+
+
+    public static MlResult<T> GetDetailException<T>(this MlErrorsDetails source) where T : Exception => source.GetDetail<T>(EX_DESC_KEY);
+    public static Task<MlResult<T>> GetDetailExceptionAsync<T>(this MlErrorsDetails source) where T : Exception => source.GetDetail<T>(EX_DESC_KEY).ToAsync();
+    public static MlResult<Exception> GetDetailException(this MlErrorsDetails source) => source.GetDetail<Exception>(EX_DESC_KEY);
+    public static Task<MlResult<Exception>> GetDetailExceptionAsync(this MlErrorsDetails source) => source.GetDetail<Exception>(EX_DESC_KEY).ToAsync();
+}
---
+++ src/MoralesLarios.OOFP.Unit.Tests/Types/MlResultActionsErrorsDetailsTests.cs
@@ -0,0 +1,218 @@
+﻿namespace MoralesLarios.OOFP.Unit.Tests.Types;
+
+public class MlResultActionsErrorsDetailsTests
+{
+
+
+    [Fact]
+    public void GetDetail_keyNotExist_return_fail()
+    {
+        MlErrorsDetails errors1 = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { "key1", "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        MlResult<string> result = errors1.GetDetail<string>("key3");
+
+        result.IsFail.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GetDetail_keyExist_return_valid()
+    {
+        MlErrorsDetails errors1 = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { "key1", "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        MlResult<string> result = errors1.GetDetail<string>("key2");
+
+        result.IsValid.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void GetDetail_keyExist_diferentTypeOfT_return_fail()
+    {
+        MlErrorsDetails errors1 = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { "key1", "value1" },
+                                                { "key2", 7m }
+                                            }
+                                    );
+
+        MlResult<string> result = errors1.GetDetail<string>("key2");
+
+        result.IsFail.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GetDetailValue_keyExist_return_valid()
+    {
+        MlErrorsDetails errors1 = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { VALUE_KEY, "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        MlResult<string> result = errors1.GetDetailValue<string>();
+
+        result.IsValid.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void GetDetailValue_keyNotExist_return_fail()
+    {
+        MlErrorsDetails errors1 = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { "key1", "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        MlResult<string> result = errors1.GetDetailValue<string>();
+
+        result.IsFail.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public async Task Tests()
+    {
+        MlResult<int> partialResult = await "Error".ToMlResultFailAsync<int>();
+
+        var data = await partialResult.CompleteWithDetailsValueIfFailAsync(69)
+                                            .BindIfFailAsync(errorsDetails => errorsDetails
+                                                                                    .GetDetailValueAsync<int>());
+        data.Should().NotBeNull();
+    }
+
+
+    [Fact]
+    public void MergeErrorsDetailsIfFail_sourceValid_secuondaryFal_return_source()
+    {
+        MlResult<int> partialResult = 1;
+
+        MlResult<int> secondary = "Error".ToMlResultFail<int>();
+
+        MlResult<int> result = partialResult.MergeErrorsDetailsIfFail(secondary);
+
+        result.ToString().Should().Be(partialResult.ToString());
+    }
+
+
+    [Fact]
+    public void MergeErrorsDetailsIfFail_sourceFAil_secondaryValid_return_source()
+    {
+        MlResult<int> partialResult = "Error".ToMlResultFail<int>();
+
+        MlResult<int> secondary = 1;
+
+        MlResult<int> result = partialResult.MergeErrorsDetailsIfFail(secondary);
+
+        result.ToString().Should().Be(partialResult.ToString());
+    }
+
+
+
+    [Fact]
+    public void MergeErrorsDetailsIfFailDioferentTypes_sourceValid_secuondaryFal_return_source()
+    {
+        MlResult<int> partialResult = 1;
+
+        MlResult<string> secondary = "Error".ToMlResultFail<string>();
+
+        MlResult<int> result = partialResult.MergeErrorsDetailsIfFailDiferentTypes(secondary);
+
+        result.ToString().Should().Be(partialResult.ToString());
+    }
+
+    [Fact]
+    public void MergeErrorsDetailsIfFailDioferentTypes_sourceFAil_secondaryValid_return_source()
+    {
+        MlResult<int> partialResult = "Error".ToMlResultFail<int>();
+
+        MlResult<string> secondary = "hola";
+
+        MlResult<int> result = partialResult.MergeErrorsDetailsIfFailDiferentTypes(secondary);
+
+        result.ToString().Should().Be(partialResult.ToString());
+    }
+
+
+
+
+
+    #region MergeErrorsDetails
+
+
+    [Fact]
+    public void MergeErrorsDetails_secondaryValid_return_source()
+    {
+        MlErrorsDetails source = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { VALUE_KEY, "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        MlResult<int> secondary = 1;
+
+        MlResult<string> result = source.MergeErrorsDetails<int, string>(secondary);
+
+        MlResult<string> expected = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { VALUE_KEY, "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        result.ToString().Should().Be(expected.ToString());
+    }
+
+    [Fact]
+    public void MergeErrorsDetails_secondaryfail_return_failMergeErrorsDetails()
+    {
+        MlErrorsDetails source = ("miError", new Dictionary<string, object> 
+                                            { 
+                                                { VALUE_KEY, "value1" },
+                                                { "key2", "value2" }
+                                            }
+                                    );
+
+        MlResult<int> secondary = ("miError2", new Dictionary<string, object> 
+                                            { 
+                                                { "other", "value1-2" },
+                                                { "key2-2", "value2-2" }
+                                            }
+                                    );
+
+        MlResult<string> result = source.MergeErrorsDetails<int, string>(secondary);
+
+        MlResult<string> expected = (["miError", "miError2"], new Dictionary<string, object> 
+                                            { 
+                                                { VALUE_KEY, "value1" },
+                                                { "key2", "value2" },
+                                                { "other", "value1-2" },
+                                                { "key2-2", "value2-2" }
+                                            }
+                                    );
+
+        result.ToString().Should().Be(expected.ToString());
+    }
+
+
+
+
+    #endregion
+
+
+
+
+}
